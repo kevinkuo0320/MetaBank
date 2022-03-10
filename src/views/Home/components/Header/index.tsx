a4182a3dc0e72cac2d2e4d16cdf5d3419fd75e1e
Kevin Kuo <63287696+kevinkuo0320@users.noreply.github.com>
2022-03-10 23:45:58 +0800

    update responsive header 2

--- src/views/Home/components/Header/index.tsx
+++ src/views/Home/components/Header/index.tsx
@@ -24,57 +24,61 @@ function Header() {
             <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
                 <img src="./metabank_logo_colored_transparent.png" style={{ height: 50, width: 69, padding: 10 }} />
                 <p className="fontMetaBank">MetaBank</p>
+
                 {/*<button className="fontOthers" onClick={() => history.push("/stake")}>
                     Stake
                 </button>*/}
-                {/* <button
-                    className="fontOthers"
-                    onClick={e => {
-                        e.preventDefault();
-                        window.open("https://metabank-finance.gitbook.io/metabank-finance-litepaper/");
-                    }}
-                >
-                    Documents
-                </button>
+                <div className="web-buttons">
+                    <button
+                        className="fontOthers"
+                        onClick={e => {
+                            e.preventDefault();
+                            window.open("https://metabank-finance.gitbook.io/metabank-finance-litepaper/");
+                        }}
+                    >
+                        Documents
+                    </button>
 
-                <button
-                    className="fontOthers"
-                    onClick={e => {
-                        e.preventDefault();
-                        window.open("https://twitter.com/metabankfinance");
-                    }}
-                >
-                    Twitter
-                </button>
+                    <button
+                        className="fontOthers"
+                        onClick={e => {
+                            e.preventDefault();
+                            window.open("https://twitter.com/metabankfinance");
+                        }}
+                    >
+                        Twitter
+                    </button>
 
-                <button
-                    className="fontOthers"
-                    onClick={e => {
-                        e.preventDefault();
-                        window.open("https://dsc.gg/metabankfinance");
-                    }}
-                >
-                    Discord
-                </button>*/}
+                    <button
+                        className="fontOthers"
+                        onClick={e => {
+                            e.preventDefault();
+                            window.open("https://dsc.gg/metabankfinance");
+                        }}
+                    >
+                        Discord
+                    </button>
 
-                {/* <button
-                    className="fontOthers"
-                    onClick={e => {
-                        e.preventDefault();
-                        window.open("https://www.youtube.com/watch?v=fT1hxDdLrQE");
-                    }}
-                >
-                    Media
-                </button>*/}
+                    {/*<button
+                        className="fontOthers"
+                        onClick={e => {
+                            e.preventDefault();
+                            window.open("https://www.youtube.com/watch?v=fT1hxDdLrQE");
+                        }}
+                    >
+                        Media
+                    </button>
 
-                {/* <button className="fontOthers"
-                    onClick={e => {
-                        e.preventDefault();
-                        window.location.href = "https://youtu.be/fT1hxDdLrQE";
-                    }}
-                   >
-                    Media
-                </button>*/}
+                    <button
+                        className="fontOthers"
+                        onClick={e => {
+                            e.preventDefault();
+                            window.location.href = "https://youtu.be/fT1hxDdLrQE";
+                        }}
+                    >
+                        Media
+                    </button>*/}
+                </div>
             </div>
             <div className="landing-header-nav-wrap">
                 <Box component="div" onMouseEnter={e => handleClick(e)} onMouseLeave={e => handleClick(e)}>
