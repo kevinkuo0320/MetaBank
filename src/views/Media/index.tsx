aa7856fb3f09732a5022dac9af74116834f84a65
Kevin Kuo <63287696+kevinkuo0320@users.noreply.github.com>
2022-03-11 03:39:14 +0800

    add video

--- src/views/Media/index.tsx
+++ src/views/Media/index.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./media.scss";
-import video from "../../assets/METABANK_1.mp4";
+import Video from "../../assets/METABANK_1.mp4";
+import { ReactVideoPlay, VideoSourceType } from "react-video-play";
 
-function Media() {
+const Media = () => {
+    let src = [
+        {
+            name: "1080p",
+            source: [
+                {
+                    source: "eh5v.files/html5video/METABANK_1.mp4",
+                    type: VideoSourceType.video_mp4,
+                },
+            ],
+        },
+        {
+            name: "720p",
+            default: true,
+            source: [
+                {
+                    source: Video,
+                    type: VideoSourceType.video_mp4,
+                },
+            ],
+        },
+    ];
+
     return (
-        <div>
-            {/*   <video width="750" height="500" controls>
-                <source src={video} type="video/mp4" />
-            </video>*/}
+        <div className="container">
+            <ReactVideoPlay sources={src} autoplay={true} />
         </div>
     );
-}
+};
 
 export default Media;
