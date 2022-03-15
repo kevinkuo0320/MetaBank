a44df42ce44f814074e5c8fd3a39555926c1592d
Kevin Kuo <63287696+kevinkuo0320@users.noreply.github.com>
2022-03-16 00:56:58 +0800

    add time

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -6,12 +6,7 @@ import CountDownTimer from "./Countdown";
 function Timer() {
     const hoursMinSecs = { hours: 48, minutes: 59, seconds: 59 };
 
-    return (
-        <div className="Timer">
-            {/*<CountDownTimer hoursMinSecs={hoursMinSecs} />*/}
-            Coming Soon
-        </div>
-    );
+    return <div className="Timer">{/*<CountDownTimer hoursMinSecs={hoursMinSecs} />*/}1 APRIL 2022, 9AM EST</div>;
 }
 
 export default Timer;
